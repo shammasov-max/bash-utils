29e2f5ebe232afc622d818af608fbcdc31408eb8
Maxim Shammasov <miramaxis@yandex.ru>
2024-11-30 21:31:48 +0300

    Create install-mongodb-ubuntu24.sh

---
+++ install-mongodb-ubuntu24.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+sudo apt update
+sudo apt install gnupg wget apt-transport-https ca-certificates software-properties-common  -y
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt update
+sudo apt install mongodb-org -y
+sudo systemctl enable mongod.service
+sudo systemctl status mongod.service
