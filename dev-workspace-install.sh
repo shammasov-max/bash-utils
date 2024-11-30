31a538ad789bb3b00d4382ba6a1b72047dd08768
Maxim Shammasov <miramaxis@yandex.ru>
2024-12-01 01:33:29 +0300

    Update dev-workspace-install.sh

--- dev-workspace-install.sh
+++ dev-workspace-install.sh
@@ -8,8 +8,8 @@ apt install -y curl wget ncdu nano git
 apt install -y build-essential checkinstall libssl-dev
 apt install -y libjpeg-dev libpng-dev libtiff-dev libgif-dev libwebp-dev 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
@@ -19,7 +19,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 curl -sL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh
 bash /tmp/nodesource_setup.sh
