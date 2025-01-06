be4b922c3641e3babca5aeca8551c592bb58d5fe
Maxim Shammasov <miramaxis@yandex.ru>
2025-01-06 06:42:21 +0300

    Update dev-workspace-install.sh

--- dev-workspace-install.sh
+++ dev-workspace-install.sh
@@ -15,7 +15,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
 
 # Add the repository to Apt sources:
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo "jammy") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
